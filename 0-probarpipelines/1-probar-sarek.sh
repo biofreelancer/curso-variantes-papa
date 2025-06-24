40fa1be8fe1ca503151cf20825a92071797c2943
BioFreelancer <89655045+biofreelancer@users.noreply.github.com>
2025-06-23 22:24:21 -0600

    Update 1-probar-sarek.sh

--- 0-probarpipelines/1-probar-sarek.sh
+++ 0-probarpipelines/1-probar-sarek.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 nextflow run nf-core/sarek \
-	-profile test,docker \
-	-r 3.5.1 \
-	--outdir prueba-sarek
+        -profile test,docker \
+        -r 3.5.1 \
+        --outdir prueba-sarek \
+        -c a-configfiles/low-res-machine.config \
+        -resume
