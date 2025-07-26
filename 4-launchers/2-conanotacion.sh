24dfe4dad228c257e4c22b9f5ec95c3904c5abce
BioFreelancer <89655045+biofreelancer@users.noreply.github.com>
2025-07-26 12:19:08 -0600

    Update 2-conanotacion.sh

--- 4-launchers/2-conanotacion.sh
+++ 4-launchers/2-conanotacion.sh
@@ -4,5 +4,6 @@ nextflow run nf-core/sarek \
     --outdir results/1000genomas \
     --genome 'GATK.GRCh38' \
     --tools strelka,vep \
+    -c a-configfiles/hi-res-machine.config \
     -profile docker \
     -resume
